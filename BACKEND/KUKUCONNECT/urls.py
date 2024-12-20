01d939a63591e6f6a5c68fe445c10e257405e664
voste126 <steveaustine126@gmail.com>
2024-12-21 00:38:22 +0300

    FEAT:MPESA INTERGRATION

--- BACKEND/KUKUCONNECT/urls.py
+++ BACKEND/KUKUCONNECT/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('api/users/', include('users.urls')),  # Your user app
     path('api/profiles/', include('profiles.urls')),  # Your profiles app
     path('api/products/', include('products.urls')),  # Your products app
+    path('api/payments/', include('payments.urls')),  # Your payments app
     # Swagger URLs
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
