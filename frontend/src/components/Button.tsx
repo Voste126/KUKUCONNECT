e4df66adbf98467ae045f4fc08333c374403df2e
voste126 <steveaustine126@gmail.com>
2024-12-23 18:18:35 +0300

    FEAT:working on the Appshell

---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,16 @@
+
+import '@mantine/core/styles.css';
+import { Button } from '@mantine/core';
+// import { useDisclosure } from '@mantine/hooks';
+
+function ButtonComponent() {
+  
+  
+  return (
+    <>
+      <Button>Button component</Button>
+    </>
+  )
+}
+
+export default ButtonComponent
