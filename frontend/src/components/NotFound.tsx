5887e0373ef306610d8147abdf4356f3b541f3cd
voste126 <steveaustine126@gmail.com>
2024-12-24 16:19:19 +0300

    starting a fresh

--- frontend/src/components/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-function NotFound(){
-    return (
-        <>
-            <h1>404 - Not Found!</h1>
-            <p>THe page You're looking for doesn't exist.</p>
-        </>
-    )
-}
-export default NotFound;
