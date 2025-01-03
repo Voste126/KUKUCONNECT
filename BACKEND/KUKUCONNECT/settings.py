a223667574164be806e1fe8b97ffa3667c486b65
voste126 <steveaustine126@gmail.com>
2025-01-03 11:23:51 +0300

    FIX:Adjusting TOKEN lifetime

--- BACKEND/KUKUCONNECT/settings.py
+++ BACKEND/KUKUCONNECT/settings.py
@@ -89,6 +89,8 @@ WSGI_APPLICATION = 'KUKUCONNECT.wsgi.application'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://localhost:3000",
+    "https://kukuconnect-frontend.onrender.com",
 ]
 
 
@@ -194,8 +196,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=50000),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1440),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
